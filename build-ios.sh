e60f1726871819db56f5b761725442706644c59d
Ezequiel Ramos <ezequielmr94@gmail.com>
2026-04-01 20:04:15 -0300

    adding manual built onnx version for iOS simulator

--- build-ios.sh
+++ build-ios.sh
@@ -18,22 +18,29 @@ cmake -B $BUILD_DIR/device \
 
 cmake --build $BUILD_DIR/device --config Release
 
-# echo "Building iOS simulator..."
+echo "Building iOS simulator..."
 
-# cmake -B $BUILD_DIR/simulator \
-#     -DCMAKE_SYSTEM_NAME=iOS \
-#     -DCMAKE_OSX_ARCHITECTURES=arm64 \
-#     -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
-#     -DCMAKE_OSX_SYSROOT=iphonesimulator
+cmake -B $BUILD_DIR/simulator \
+    -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
+    -DCMAKE_OSX_SYSROOT=iphonesimulator
 
-# cmake --build $BUILD_DIR/simulator --config Release
+cmake --build $BUILD_DIR/simulator --config Release
 
 libtool -static \
   $BUILD_DIR/device/liblpr_sdk_static.a \
   third_party/onnxruntime/ios/onnxruntime.xcframework/ios-arm64/libonnxruntime.a \
   -o $BUILD_DIR/device/liblpr_combined.a
 
+libtool -static \
+  $BUILD_DIR/simulator/liblpr_sdk_static.a \
+  third_party/onnxruntime/ios/onnxruntime.xcframework/ios-arm64-simulator/libonnxruntime.a \
+  -o $BUILD_DIR/simulator/liblpr_combined.a
+
 xcodebuild -create-xcframework \
  -library $BUILD_DIR/device/liblpr_combined.a \
+ -headers include \
+ -library $BUILD_DIR/simulator/liblpr_combined.a \
  -headers include \
  -output $BUILD_DIR/LprSdk.xcframework
